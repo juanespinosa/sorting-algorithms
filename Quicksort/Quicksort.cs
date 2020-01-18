7b88528ff3b652169197f36ff7d45cc4e5354b46
Juan Espinosa <juange88@gmail.com>
2020-01-18 07:01:36 -0500

    Added some docs

--- Quicksort/Quicksort.cs
+++ Quicksort/Quicksort.cs
@@ -1,15 +1,24 @@
 ﻿using SortingAlgorithms.Utils;
-using System;
 
 namespace Quicksort
 {
     public class QuicksortAlgorithm
     {
+        /// <summary>
+        /// Calls the quicksort implementation
+        /// </summary>
+        /// <param name="A">Array to be sorted</param>
         public void Sort(int[] A)
         {
             Quicksort(A, 1, A.Length);
         }
 
+        /// <summary>
+        /// Quicksort implementation
+        /// </summary>
+        /// <param name="A">Array to be sorted</param>
+        /// <param name="p">first position</param>
+        /// <param name="r">last position</param>
         private void Quicksort(int[] A, int p, int r)
         {
             if(p < r)
@@ -20,6 +29,14 @@ namespace Quicksort
             }
         }
 
+        /// <summary>
+        /// Create 2 subsets using a pivot value to split
+        /// the array
+        /// </summary>
+        /// <param name="A">The array to be splitted</param>
+        /// <param name="p">first element position of the subset</param>
+        /// <param name="r">last element position of the subset</param>
+        /// <returns>First position of the second subset</returns>
         private int Partition(int[] A, int p, int r)
         {
             var x = A[r - 1]; // pivot
