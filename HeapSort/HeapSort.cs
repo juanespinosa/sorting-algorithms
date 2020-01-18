52b2391a103d5c573263e5bcddacbc8a1f931be2
Juan Espinosa <juange88@gmail.com>
2020-01-18 00:16:32 -0500

    Added a functional implementation of a Heap using a PriorityQueue

--- HeapSort/HeapSort.cs
+++ HeapSort/HeapSort.cs
@@ -26,7 +26,7 @@
         /// <param name="A">Array with the elements</param>
         /// <param name="i">position</param>
         /// <param name="n">Heap size</param>
-        private void MaxHeapify(int[] A, int i, int n)
+        public void MaxHeapify(int[] A, int i, int n)
         {
             var l = Left(i);
             var r = Right(i);
@@ -53,13 +53,33 @@
         }
 
         /// <summary>
+        /// Exchange two positions in the array
+        /// </summary>
+        /// <param name="A">The array with the elements</param>
+        /// <param name="posA">Position A to swap</param>
+        /// <param name="posB">Position B to Swap</param>
+        public void Exchange(int[] A, int posA, int posB)
+        {
+            var old = A[posA - 1];
+            A[posA - 1] = A[posB - 1];
+            A[posB - 1] = old;
+        }
+
+        /// <summary>
+        /// Finds the parent position in an array (the one in the middle)
+        /// </summary>
+        /// <param name="i">Length of the array</param>
+        /// <returns>Parent position</returns>
+        public int Parent(int i) => i / 2;
+
+        /// <summary>
         /// Create a Heap with the highest (max) number as parent
         /// </summary>
         /// <param name="A">Array with the elements</param>
         private void BuildMaxHeap(int[] A)
         {
             var heapSize = A.Length;
-            for(int i = A.Length / 2; i >= 1; i--)
+            for(int i = Parent(A.Length); i >= 1; i--)
             {
                 MaxHeapify(A, i, heapSize);
             }
@@ -78,18 +98,5 @@
         /// <param name="i">Parent position</param>
         /// <returns>Right position</returns>
         private int Right(int i) => 2 * i + 1;
-
-        /// <summary>
-        /// Exchange two positions in the array
-        /// </summary>
-        /// <param name="A">The array with the elements</param>
-        /// <param name="posA">Position A to swap</param>
-        /// <param name="posB">Position B to Swap</param>
-        private void Exchange(int[] A, int posA, int posB)
-        {
-            var old = A[posA - 1];
-            A[posA - 1] = A[posB - 1];
-            A[posB - 1] = old;
-        }
     }
 }
---
+++ PriorityQueue/PriorityQueue.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace PriorityQueue
+{
+    public class PriorityQueue
+    {
+        private int[] _A;
+
+        private readonly HeapSort.HeapSort _hs;
+        
+        public PriorityQueue()
+        {
+            _A = new int[0];
+            _hs = new HeapSort.HeapSort();
+        }
+
+        /// <summary>
+        /// Returns the Maximum priority in the queue
+        /// </summary>
+        /// <returns>int with the max priority</returns>
+        public int GetMaximum() 
+        {
+            if (_A.Length < 1) throw new IndexOutOfRangeException("heap size is 0");
+            return _A[0]; 
+        }
+
+        /// <summary>
+        /// Returns and removes the Maximum priority in the queue
+        /// </summary>
+        /// <returns>int with the max priority</returns>
+        public int ExtractMaximum()
+        {
+            if (_A.Length < 1) throw new IndexOutOfRangeException("heap size is 0");
+
+            var max = _A[0];
+            _A[0] = _A[_A.Length - 1];
+            Array.Resize(ref _A, _A.Length - 1);
+            _hs.MaxHeapify(_A, 1, _A.Length);
+            return max;
+        }
+
+        /// <summary>
+        /// Increase a position of the queue with a new 
+        /// value "key"
+        /// </summary>
+        /// <param name="i">Position to be updated</param>
+        /// <param name="key">New value</param>
+        public void IncreaseKey(int i, int key)
+        {
+            if (key < _A[i - 1]) throw new ArgumentException($"new key ({key}) is smaller than the current key ({_A[i - 1]})");
+            _A[i - 1] = key;
+            while(i > 1 && _A[_hs.Parent(i) - 1] < _A[i - 1])
+            {
+                _hs.Exchange(_A, i, _hs.Parent(i));
+                i = _hs.Parent(i);
+            }
+        }
+            
+        /// <summary>
+        /// Insert a new key into the queue
+        /// </summary>
+        /// <param name="key">Value to be inserted</param>
+        public void Insert(int key)
+        {
+            Array.Resize(ref _A, _A.Length + 1);
+            _A[_A.Length - 1] = int.MinValue;
+            IncreaseKey(_A.Length, key);
+        }
+
+        /// <summary>
+        /// Returns the queue
+        /// </summary>
+        /// <returns>The queue</returns>
+        public int[] GetQueue() => _A;
+    }
+}
