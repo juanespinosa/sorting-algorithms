3e555b1edd4b76dcb86260302902b01b6b3c4a6b
Juan Espinosa <juange88@gmail.com>
2020-01-18 09:01:50 -0500

    Create an abstract class in order to implement multiple kinds of quicksort

--- SortingAlgorithms.Quicksort/Quicksort.cs
+++ SortingAlgorithms.Quicksort/Quicksort.cs
@@ -2,17 +2,8 @@
 
 namespace Quicksort
 {
-    public class Quicksort
+    public class Quicksort : UnpartitionedQuicksort
     {
-        /// <summary>
-        /// Calls the quicksort implementation
-        /// </summary>
-        /// <param name="A">Array to be sorted</param>
-        public void Sort(int[] A)
-        {
-            Sort(A, 1, A.Length);
-        }
-
         /// <summary>
         /// Create 2 subsets using a pivot value to split
         /// the array
@@ -21,7 +12,7 @@ namespace Quicksort
         /// <param name="p">first element position of the subset</param>
         /// <param name="r">last element position of the subset</param>
         /// <returns>First position of the second subset</returns>
-        public virtual int Partition(int[] A, int p, int r)
+        public override int Partition(int[] A, int p, int r)
         {
             var x = A[r - 1]; // pivot
             var i = p - 1;
@@ -35,22 +26,6 @@ namespace Quicksort
             }
             Common.Exchange(A, i + 1, r);
             return i + 1;
-        }
-
-        /// <summary>
-        /// Quicksort implementation
-        /// </summary>
-        /// <param name="A">Array to be sorted</param>
-        /// <param name="p">first position</param>
-        /// <param name="r">last position</param>
-        private void Sort(int[] A, int p, int r)
-        {
-            if(p < r)
-            {
-                var q = Partition(A, p, r);
-                Sort(A, p, q - 1);
-                Sort(A, q + 1, r);
-            }
         }
     }
 }
---
+++ SortingAlgorithms.Utils/UnpartitionedQuicksort.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortingAlgorithms.Utils
+{
+    public abstract class UnpartitionedQuicksort
+    {
+        /// <summary>
+        /// Calls the quicksort implementation
+        /// </summary>
+        /// <param name="A">Array to be sorted</param>
+        public void Sort(int[] A)
+        {
+            Sort(A, 1, A.Length);
+        }
+
+        /// <summary>
+        /// Create 2 subsets using a pivot value to split
+        /// the array
+        /// </summary>
+        /// <param name="A">The array to be splitted</param>
+        /// <param name="p">first element position of the subset</param>
+        /// <param name="r">last element position of the subset</param>
+        /// <returns>First position of the second subset</returns>
+        public abstract int Partition(int[] A, int p, int r);
+
+        /// <summary>
+        /// Quicksort implementation
+        /// </summary>
+        /// <param name="A">Array to be sorted</param>
+        /// <param name="p">first position</param>
+        /// <param name="r">last position</param>
+        private void Sort(int[] A, int p, int r)
+        {
+            if (p < r)
+            {
+                var q = Partition(A, p, r);
+                Sort(A, p, q - 1);
+                Sort(A, q + 1, r);
+            }
+        }
+    }
+}
